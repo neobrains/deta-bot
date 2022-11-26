f173d4dbafbf047503a10cf0fe09b5234dbb0bf7
jnsougata <jnsougata@gmail.com>
2022-11-26 22:39:03 +0530

    updated import

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
-import discohook.discohook as dh
+import discohook.discohook as discohook
 
-app = dh.Client(
+app = discohook.Client(
     application_id=int(os.getenv("APPLICATION_ID")),
     public_key=os.getenv("PUBLIC_KEY"),
     token=os.getenv("DISCORD_TOKEN"),
