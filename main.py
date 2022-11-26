2a0097372f5f7cd91f6e9717f0d136835e3cbdd5
Sofa <Nextil3o3@gmail.com>
2022-11-26 16:55:24 +0100

    added env

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+import discohook.discohook as discohook
+
+app = discohook.Client(
+    application_id=int(os.getenv("APPLICATION_ID")),
+    public_key=os.getenv("PUBLIC_KEY"),
+    token=os.getenv("DISCORD_TOKEN"),
+    express_debug=True,
+)
